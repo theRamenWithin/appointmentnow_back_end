c9f04a1b243053633227ef1fa7736ed2f8511b6f
Gavin Simms <me@gavinsimms.com>
2021-01-29 10:50:38 +1100

    added basic seed data for faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,67 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+100.times do 
+    addresses = Address.new(
+        addressLine1: Faker::Address.street_address,
+        addressLine2: Faker::Address.secondary_address,
+        city: Faker::Address.city,
+        state: Faker::Address.state,
+        country: Faker::Address.country,
+        postcode: Faker::Address.postcode
+    )
+    
+    addresses.save
+
+end
+# Bookings, how do I fake the user and event bit?
+# 100.times do
+#     bookings = Booking.new(
+#         startTime:
+#         endTime:
+#         multipleCustomers:
+#         customerLimit:
+
+#     )
+
+#     bookings.save
+
+# end
+# likewise, how do I save the user bit
+# 100.times do
+#     events = Event.new(
+#         title:
+#         description:
+#         date:
+#     )
+    
+#     event.save
+
+# end
+
+# 100.times do
+#     organizations = Organization.new(
+#         organizationName: Faker::Company.name,
+#         description: Faker::Company.catch_phrase,
+#         phone: Faker::PhoneNumber.phone_number,
+#         email: Faker::Internet.email
+#     )
+    
+#     organizations.save
+
+# end
+
+# 100.times do
+#     users = Users.new(
+#         firstName: Faker::Name.first_name,
+#         lastName: Faker::Name.last_name,
+#         mobile: Faker::PhoneNumber.cell_phone,
+#         email: Faker::Internet.email
+
+#     )
+
+# end
+
+# 100.times do 
+
+# end
+
