da38965b2f80997d186afb2c9e99a71d8506b19b
Jackson <jackson@192-168-1-105.tpgi.com.au>
2021-01-29 17:17:37 +1100

    Updated user signup routes/puma config.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,8 @@ class User < ApplicationRecord
     has_one :address
     has_secure_password
 
-    validates :firstName, presence: true
-    validates :lastName, presence: true
-    validates :mobile, presence: true
-    validates :mobile, uniqueness: true
-    validates :email, presence: true
-    validates :email, uniqueness: true
+    validates :username, presence: true, uniqueness: true
+    validates :mobile, presence: true, uniqueness: true
+    validates :email, uniqueness: true, presence: true
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-    end
 end
