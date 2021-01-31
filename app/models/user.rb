d7cd81f45601c91b3d63e7054fa0c1e98efac017
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2021-01-31 15:52:27 +1100

    Removed requirement for mobile number

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :username, presence: true, uniqueness: true
-    validates :mobile, presence: true, uniqueness: true
     validates :email, uniqueness: true, presence: true
+    
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
     end
