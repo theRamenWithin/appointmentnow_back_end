ae250fcc14072e5c28584b19d70bc9f7c3b162f8
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2021-01-31 15:53:42 +1100

    Add username field to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_29_034131) do
+ActiveRecord::Schema.define(version: 2021_01_31_003837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 2021_01_29_034131) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "address_id", null: false
+    t.string "username"
     t.index ["address_id"], name: "index_users_on_address_id"
   end
 
