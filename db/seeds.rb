ec4fe8ee8cc321d94f1cce86f926eb5e2bc9576d
Gavin Simms <me@gavinsimms.com>
2021-02-01 14:41:27 +1100

    added faker data for x2 users handyandy (admin) and offsider (manager)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require "faker"
 
+# Creation of Fake Users and Addresses
 20.times do |i|
     @address = Address.new(
         addressLine1: Faker::Address.street_address,
@@ -34,4 +35,78 @@ require "faker"
     @organization.save
     puts "#{i} organization created"
 end
+# Creation of Fake Admin: Handy Andy
+1.times do |i|
+    @address = Address.new(
+        addressLine1: '1 Test Way',
+        addressLine2: 'Unit 1',
+        city: 'London',
+        state: 'England',
+        country: 'United Kingdom',
+        postcode: 'W1 9AH'
+    )
+    @address.save
+    puts "#{i} address created"
 
+    @user = User.new(
+        firstName: 'Handy',
+        lastName: 'Andy',
+        mobile: 55555555,
+        email: 'handyandy@gmail.com',
+        password_digest: 'password',
+        username: 'handyandy',
+        address_id: @address.id
+    )
+    @user.save
+    puts "#{i} user created"
+
+    @organization = Organization.new(
+        organizationName: 'Handy Andy Fixes',
+        description: 'Handy Andy Fixes Stuff PTY LTD',
+        phone: 55555554,
+        email: 'handyandybusiness@gmail.com',
+        address_id: @address.id,
+    )
+    @organization.save
+    puts "#{i} organization created"
+
+    @organization_role = OrganizationRole.new(
+        organization_id: @organization.id,
+        user_id: @user.id,
+        role: 2
+    )
+    @organization_role.save
+    puts "#{i} organization role created"
+end
+# Creation of Fake Manager
+1.times do |i|
+    @address = Address.new(
+        addressLine1: Faker::Address.street_address,
+        addressLine2: Faker::Address.secondary_address,
+        city: Faker::Address.city,
+        state: Faker::Address.state,
+        country: Faker::Address.country,
+        postcode: Faker::Address.postcode,
+    )
+    @address.save
+    puts "#{i} address created"
+
+    @user = User.new(
+        firstName: 'Offsider',
+        lastName: 'Andy',
+        mobile: 55555555,
+        email: 'offsiderhandyandy@gmail.com',
+        password_digest: 'password',
+        username: 'offsider',
+        address_id: @address.id
+    )
+    @user.save
+
+    @organization_role = OrganizationRole.new(
+        organization_id: 21,
+        user_id: @user.id,
+        role: 1
+    )
+    @organization_role.save
+    puts "#{i} organization role created"
+end
