5b0e273d894e7e9b84fd99719956c498696d2d4f
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2021-02-02 23:15:04 +1100

    Renamed default role

--- app/models/organization_role.rb
+++ app/models/organization_role.rb
@@ -1,5 +1,5 @@
 class OrganizationRole < ApplicationRecord
   belongs_to :organization
   belongs_to :user
-  enum role: {admin: 2, manager: 1, provider: 0}
+  enum role: {admin: 2, manager: 1, default: 0}
 end
