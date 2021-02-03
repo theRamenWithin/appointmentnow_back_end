539fee71c6e42cfd872a28ae8540f5108ea45428
Gavin Simms <me@gavinsimms.com>
2021-02-03 12:56:27 +1100

    second rspec test for organization

---
+++ spec/models/organization_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+# TEST FOR ORGANIZATION CREATION
+RSpec.describe Organization, type: :model do
+    context 'New Organization' do
+        it 'must create an Organization' do
+            1.times do |i|
+                @address = Address.new(
+                    address_line_1: '1 Test Way',
+                    address_line_2: 'Unit 1',
+                    city: 'London',
+                    state: 'England',
+                    country: 'United Kingdom',
+                    postcode: 'W1 9AH'
+                )
+                @address.save
+                puts "#{i + 1} address created"
+            
+                @organization = Organization.new(
+                    organization_name: 'Organization Test',
+                    description: 'Handy Andy Fixes Stuff PTY LTD',
+                    phone: 55555554,
+                    email: 'handyandybusiness@gmail.com',
+                    address_id: @address.id,
+                )
+                @organization.save
+                puts "#{i + 1} organization created"
+            end
+            expect(Organization.last.organization_name).to eq('Organization Test')
+        end
+    end
+end
