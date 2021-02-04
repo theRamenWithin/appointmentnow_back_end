54ded9c5ca2fffc13bbc670623a67b6a5c89f073
Gavin Simms <me@gavinsimms.com>
2021-02-04 12:02:21 +1100

    added website to organizations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_094536) do
+ActiveRecord::Schema.define(version: 2021_02_04_010031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 2021_02_02_094536) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "address_id", null: false
+    t.string "website"
     t.index ["address_id"], name: "index_organizations_on_address_id"
   end
 
