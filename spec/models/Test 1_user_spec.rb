421392a5d58724cbaec15958c03f586686ee6481
Gavin Simms <me@gavinsimms.com>
2021-02-08 11:59:56 +1100

    updated rspec tests

--- spec/models/Test 1_user_spec.rb
+++ spec/models/Test 1_user_spec.rb
@@ -4,29 +4,31 @@ RSpec.describe User, type: :model do
     context 'New User' do
         it 'must create a user' do
             1.times do |i|
-                @address = Address.new(
-                    address_line_1: '1 Test Way',
-                    address_line_2: 'Unit 1',
-                    city: 'London',
-                    state: 'England',
-                    country: 'United Kingdom',
-                    postcode: 'W1 9AH'
-                )
-                @address.save
-                puts "#{i + 1} address created"
-            
                 @user = User.new(
                     first_name: 'Test',
                     last_name: 'RSpec',
                     mobile: 55555555,
                     email: 'rspec@gmail.com',
                     password_digest: User.digest('password'),
-                    username: 'rspec',
-                    address_id: @address.id
+                    username: 'rspec'
                 )
                 @user.save
-                puts "#{i + 1} user created"
+                # puts "#{i + 1} user created"
             end
+                
+                @address = Address.new(
+                    address_line_1: '1 Test Way',
+                    address_line_2: 'Unit 1',
+                    city: 'London',
+                    state: 'England',
+                    country: 'United Kingdom',
+                    postcode: 'W1 9AH',
+                    user_id: @user.id
+                )
+                @address.save
+                # puts "#{i + 1} address created"
+            
+
             expect(User.last.first_name).to eq('Test')
         end
     end
--- spec/models/Test_2_organization_spec.rb
+++ spec/models/Test_2_organization_spec.rb
@@ -4,27 +4,27 @@ RSpec.describe Organization, type: :model do
     context 'New Organization' do
         it 'must create an Organization' do
             1.times do |i|
-                @address = Address.new(
-                    address_line_1: '1 Test Way',
-                    address_line_2: 'Unit 1',
-                    city: 'London',
-                    state: 'England',
-                    country: 'United Kingdom',
-                    postcode: 'W1 9AH'
-                )
-                @address.save
-                puts "#{i + 1} address created"
-            
                 @organization = Organization.new(
                     organization_name: 'Organization Test',
                     description: 'Handy Andy Fixes Stuff PTY LTD',
                     phone: 55555554,
                     email: 'handyandybusiness@gmail.com',
                     website: 'http://test.com',
-                    address_id: @address.id,
                 )
                 @organization.save
                 puts "#{i + 1} organization created"
+                @address = Address.new(
+                    address_line_1: '1 Test Way',
+                    address_line_2: 'Unit 1',
+                    city: 'London',
+                    state: 'England',
+                    country: 'United Kingdom',
+                    postcode: 'W1 9AH',
+                    organization_id: @organization.id
+                )
+                @address.save
+                puts "#{i + 1} address created"
+
             end
             expect(Organization.last.organization_name).to eq('Organization Test')
         end
